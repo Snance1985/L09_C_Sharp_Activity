025a3fc1eb3ea1ebb0422c12bb6adfa5f93327b9
Sean Nance <sean_nance@hotmail.com>
2023-10-28 19:37:20 -0700

    created L09 c-sharp activity files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿char[] letters = { 'W', 'e', ' ', 'a', 'r', 'e', ' ', 'B', 'S', 'T', ' ' };
+
+List<string> words = new List<string>();
+
+createWords(letters, words);
+
+foreach (string w in words) {
+    Console.WriteLine(w);
+}
+
+void createWords(char[] letters, List<string> words)
+{
+    string temp = "";
+
+    foreach (char l in letters) {
+        if (l != ' ') {
+            temp += l;
+        } else {
+            words.Add(temp);
+            temp = "";
+        }
+    }
+}
+
